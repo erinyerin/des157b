c7a43d245dc964df3892c2648497fd0e9fba6dbc
park_erin <empark1201@gmail.com>
2026-05-26 01:52:16 -0700

    added javascript interactivity

---
+++ development/version2/script.js
@@ -0,0 +1,46 @@
+(function() {
+    'use strict'
+    let dialogueData = {};
+
+    async function loadDialogue() {
+        const result = await fetch("dialogue.json");
+        dialogueData = await result.json();
+        displayText("A1");
+    }
+
+    function displayText(key) {
+        const node = dialogueData[key];
+
+        if (node.speaker === "user") {
+            appendMessage(node.content, "sentMessage");
+            displayText(node.next);
+
+        } else {
+            appendMessage(node.content, "receivedMessage");
+            const ul = document.querySelector("#dialogueOptions ul");
+            ul.innerHTML = "";
+
+            node.options.forEach(optionKey => {
+                const optionNode = dialogueData[optionKey];
+
+                const li = document.createElement("li");
+                li.textContent = optionNode.content;
+                li.addEventListener("click", () => {
+                    appendMessage(optionNode.content, "sentMessage");
+                    displayText(optionNode.next);
+                });
+                ul.appendChild(li);
+            });
+        }
+    }
+
+    function appendMessage(text, className) {
+        const messages = document.getElementById("messages");
+        const p = document.createElement("p");
+        p.className = className;
+        p.textContent = text;
+        messages.appendChild(p);
+    }
+
+    loadDialogue();
+})()
